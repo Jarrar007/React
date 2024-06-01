a605d4625dafedfd5d98b11db337f7406cf880cd
Jarrar007 <144526121+Jarrar007@users.noreply.github.com>
2024-06-01 21:51:50 +0500

    Add files via upload

---
+++ resume/src/data/react-projects.js
@@ -0,0 +1,25 @@
+export const react_data = [
+    {
+        title : "Random Password Generator",
+        github_link : "https://github.com/Jarrar007/React/tree/main/password-generator",
+        live_view : "https://password-generator-by-jarrar.netlify.app",
+        img : "https://t3.ftcdn.net/jpg/06/04/74/88/360_F_604748806_gQSyPrazhAocHefqrUtieGBKK22PS5QZ.jpg",
+        description : "A Random password generator app using react. It includes components, hooks use of random and floor functions it also includes functionalities like copy to clipboard"
+    },
+
+    {
+        title : "Anizone App",
+        github_link : "https://github.com/Jarrar007/React/tree/main/router",
+        live_view : "https://anizone07-by-jarrar.netlify.app",
+        img : "https://wallpapercave.com/wp/wp5146356.jpg",
+        description : "An Anime App where you can watch anime , read manga and discover anime, it is based on concepts like routing, hooks, api calling and handling"
+    },
+
+    {
+        title : "Film Fusion",
+        github_link : "https://github.com/Jarrar007/React/tree/main/filmFusion",
+        live_view : "https://film-fusion-by-jarrar.netlify.app",
+        img : "https://c4.wallpaperflare.com/wallpaper/788/41/190/movie-wallpaper-preview.jpg",
+        description : "A website on which you can search any movie and watch it on a website whoose link has been embeded. this project includes concepts like Hooks,conditional rendering,Api handling,responsive UI and the whole project is made in Tailwind CSS"
+    }
+]
