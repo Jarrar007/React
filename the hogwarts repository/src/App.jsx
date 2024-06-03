57743f59a8d92318eaf49314f78385149cf0619d
Jarrar007 <144526121+Jarrar007@users.noreply.github.com>
2024-06-03 21:38:08 +0500

    Add files via upload

---
+++ the hogwarts repository/src/App.jsx
@@ -0,0 +1,28 @@
+import axios from "axios"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Nav from "./components/nav"
+import Home from "./pages/home"
+import Spells from "./pages/spells"
+import Characters from "./pages/characters"
+import Books from "./pages/books"
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Router>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/books" element={<Books />}/>
+          <Route path="/characters" element={<Characters />}/>
+          <Route path="/spells" element={<Spells />}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ the hogwarts repository/src/components/nav.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+
+const [sideBar,setSideBar] = useState(false)
+
+function sideBarOn () {
+  setSideBar (true)
+}
+
+function sideBarOff () {
+  setSideBar(false)
+}
+
+  return (
+<span>
+  <div className={` bg-red-300 ${sideBar ? 'w-56' : ' w-16'} h-full fixed mt-0 ml-0 transition-transform duration-700 ease-in`}>
+    
+    <ul className="flex flex-col items-center gap-7 mt-4">
+      <li className="cursor-pointer"><img className="w-10 h-auto hover:scale-110" src="/menu-bar.png" onClick={sideBarOn}/></li>
+      <li className={`text-2xl font-bold text-orange-600 p-2 rounded-full cursor-pointer ${sideBar && 'hover:text-black hover:bg-white'}`}><Link to='/'>{sideBar ? 'Home' : <img className="w-10 h-auto hover:scale-110" src="/home.png"/>}</Link></li>
+      <li className={`text-2xl font-bold text-orange-600 p-2 rounded-full cursor-pointer ${sideBar && 'hover:text-black hover:bg-white'}`}><Link to='/books'>{sideBar ? 'Books' : <img className="w-10 h-auto hover:scale-110" src="/book.png"/>}</Link></li>
+      <li className={`text-2xl p-2 rounded-full font-bold text-orange-600 cursor-pointer ${sideBar && 'hover:text-black hover:bg-white'}`}><Link to='/characters'>{sideBar ? 'Characters' : <img className="w-10 h-auto hover:scale-110" src="/wizard.png"/> }</Link></li>
+      <li className={`text-2xl p-2 rounded-full font-bold text-orange-600 cursor-pointer ${sideBar && 'hover:text-black hover:bg-white'}`}><Link to='/spells'>{sideBar ? 'Spells' : <img className="w-10 h-auto hover:scale-110" src="/magic-wand.png"/>}</Link></li>
+      {sideBar && <li className="cursor-pointer"><img className="w-10 h-auto hover:scale-110" src="/cancel.png" onClick={sideBarOff} /></li>}
+    </ul>
+   
+    </div>
+    </span>
+  )
+}
+
+export default Nav
