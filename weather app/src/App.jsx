ff0e59c98777b55fc7e05f4464139524ec957c0c
Jarrar007 <144526121+Jarrar007@users.noreply.github.com>
2024-07-30 22:18:55 -0700

    Add files via upload

---
+++ weather app/src/App.jsx
@@ -0,0 +1,20 @@
+import Weather from "./Weather"
+
+const App = () => {
+
+  return (
+    <div className="w-full h-[100vh] bg-center bg-cover selection:bg-cyan-400" style={{backgroundImage : `url('bg.jpg')`}}>
+      <Weather />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+//b0e9330f64f64d699c5210043243007
